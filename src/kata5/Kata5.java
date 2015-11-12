ebf4eafad10b1af7a91225d1c88c4e53dc922d17
usuario <usuario@10.22.143.9>
2015-11-12 09:34:06 +0000

    Segunda versión donde nos conectamos a la base de datos del profesor

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -22,12 +22,15 @@ public class Kata5 {
      */
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
       //Depende del gestor de bases de datos que estemos usando
-      Class.forName("org.sqlite.JDBC");
+      //Class.forName("org.sqlite.JDBC");
       //Aqui ponemos nuestra base de datos
-      Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
+      //Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
 
-      
-      
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+      //Direccion ip del host
+      //ipconfig y nosotros tenemos que poner la direccion 10.22.143.90 que
+      //es la direccion. EL puerto de oracle es siempre 1521
+      Connection c = DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl","system","orcl");
     //Permite realizar consultas
     Statement stmt = c.createStatement();
     //Aqui digo la query que quiero que ejecute
